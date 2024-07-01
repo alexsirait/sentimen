f9d15275c3dd2867fd6ad14d8e3811573bdacd63
ALEX SIRAIT <alexsirait1001@gmail.com>
2024-07-01 21:00:50 +0700

    PUSH

--- app/Http/Controllers/SentimentController.php
+++ app/Http/Controllers/SentimentController.php
@@ -17,9 +17,15 @@ class SentimentController extends Controller
 
             $translatedText = $translator->translate($req->msg);
 
+            $stringParg = preg_replace("/[^a-zA-Z\s]/", "", $translatedText);
+
+            $stringParg = strtolower($stringParg);
+
+            $stringParg = preg_replace('/\s+/', ' ', $stringParg);
+
             $analyzer = new Analyzer();
 
-            $output = $analyzer->getSentiment($translatedText);
+            $output = $analyzer->getSentiment($stringParg);
 
             return response()->json([
                 "output" => $output,
